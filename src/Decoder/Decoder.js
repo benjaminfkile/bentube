e29a98cd290dd0daaa257b8a3b953b75bd6594cb
ben <benjaminfkile@gmail.com>
2020-04-18 03:56:19 -0600

    dumbass

--- src/Decoder/Decoder.js
+++ src/Decoder/Decoder.js
@@ -5,8 +5,8 @@ class Decoder extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      serverURL: 'http://localhost:8000/downloadmp3?url=',
-      // serverURL: 'https://frozen-thicket-30265.herokuapp.com/downloadmp3?url=',
+      // serverURL: 'http://localhost:8000/downloadmp3?url=',
+      serverURL: 'https://frozen-thicket-30265.herokuapp.com/downloadmp3?url=',
       interval: 1000,
       duration: null,
       progress: 0
