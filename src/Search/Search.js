a4d72bf9903281e8a14604d8aeff2932dacdbbe9
ben <benjaminfkile@gmail.com>
2020-04-19 00:26:25 -0600

    new api key

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -7,7 +7,7 @@ class Search extends Component {
     constructor() {
         super();
         this.state = {
-            apiKey: 'AIzaSyCyDGjcu0UgCCIWV1ZRB2Tv-i3M9i1iaa0',
+            apiKey: 'AIzaSyDxOOZG9aTnOFsxkvv9dBc3SSG242dPc_4',
             searchURL: 'https://www.googleapis.com/youtube/v3/search',
             baseURL: 'https://www.youtube.com/watch?v=',
             response: null,
